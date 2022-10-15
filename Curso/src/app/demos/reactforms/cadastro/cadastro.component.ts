e664099ead512e06a9b55014df1441670036f110
Antonio Junio Pardim <antoniojrpardim@gmail.com>
2022-10-15 11:22:50 -0300

    Validação basica de formulario

--- Curso/src/app/demos/reactforms/cadastro/cadastro.component.ts
+++ Curso/src/app/demos/reactforms/cadastro/cadastro.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Usuario } from './models';
 
 @Component({
   selector: 'app-cadastro',
@@ -6,10 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class CadastroComponent implements OnInit {
+cadastroGroup: FormGroup
+usuario: Usuario
+dadosFormJson:string
 
-  constructor() { }
+constructor(private fb:FormBuilder){}
+ngOnInit()
+{
+//com formBuilder
+this.cadastroGroup = this.fb.group({
+  nome: ['', Validators.required],
+  cpf: [''],
+  email: ['', [Validators.required,Validators.email]],
+  senha: [''],
+  confirmaSenha:['']
+})
+}  
 
-  ngOnInit() {
+public adicionarUsuario(){
+  if(this.cadastroGroup.dirty && this.cadastroGroup.valid){
+    //transformando dados do formulario em objeto
+    this.usuario = Object.assign({},this.usuario,this.cadastroGroup.value)
+    //tranformando valores do formulario em JSON
+    this.dadosFormJson = JSON.stringify(this.cadastroGroup.value)
   }
+   
+}
 
 }
+
+
+//-----FormControl------//-----------//
+// this.cadastroGroup = new FormGroup({
+//   nome: new FormControl(''),
+//   cpf: new FormControl(''),
+//   email: new FormControl(''),
+//   senha: new FormControl(''),
+//   confirmaSenha: new FormControl('')
+
+//----------FIM-----------------------//
---
+++ Curso/src/app/demos/reactforms/cadastro/models.ts
@@ -0,0 +1,7 @@
+export interface Usuario {
+    nome: string;
+    email:string;
+    CPF:string;
+    senha:string;
+    confirmaSenha:string
+}
