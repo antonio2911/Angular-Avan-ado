2324e77bfa95b8a80dfdfe456b456f8fedabd523
Antonio Junio Pardim <antoniojrpardim@gmail.com>
2022-10-29 14:56:47 -0300

    Revert "Começando a separar os modules e rotas personalizadas"
    This reverts commit 60a31a7cecf511ad9ea617a7283462718a2d1c04.

--- Curso/src/app/app.routes.ts
+++ Curso/src/app/app.routes.ts
@@ -1,24 +1,11 @@
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { HomeComponent } from './navegacao/home/home.component';
 import { SobreComponent } from './institucional/sobre/sobre.component';
 import { CadastroComponent } from './demos/reactforms/cadastro/cadastro.component';
-import { NgModule } from '@angular/core';
 
-const rootRouterConfig: Routes = [
+export const rootRouterConfig: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full'},
     { path: 'home', component: HomeComponent},
     { path: 'sobre', component: SobreComponent },
-    { path: 'cadastro', component: CadastroComponent },
-    {path: 'produtos', loadChildren: () => import('./demos/arquitetura-componentes/produto.module').then(m => m.ProdutoModule)}
+    { path: 'cadastro', component: CadastroComponent }
 ];
-
-@NgModule({
-    imports: [
-        RouterModule.forRoot(rootRouterConfig)
-    ],
-    exports: [
-        RouterModule
-    ]
-})
-
-export class AppRoutingModule {}
