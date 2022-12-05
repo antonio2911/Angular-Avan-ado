663e1dbbcb7dad8912aff0c3dcf84059a6857f59
Antonio Junio Pardim <antoniojrpardim@gmail.com>
2022-12-05 20:53:59 -0300

    Iniciando testes em angular

---
+++ Curso/src/app/demos/pipesCustomizados/filesize.pipe.spec.ts
@@ -0,0 +1,9 @@
+import { FileSizePipe } from './filesize.pipe';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+describe('fileSizePipe', () => {
+  describe('testeInsolado', () => {
+    const pipe = new FileSizePipe();
+    it('Deve converter bytes para MB');
+  });
+});
